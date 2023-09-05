2fface0acadb69093b3fd1349407fff42e7984e7
Jose Echavez M <jose.echavez92@gmail.com>
2023-09-05 08:04:37 -0500

    add harpoon plug

--- mappings.lua
+++ mappings.lua
@@ -32,6 +32,24 @@ return {
     ["<leader>b"] = { name = "Buffers" },
     -- quick save
     -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
+    ["<leader>a"] = {
+      function() require("harpoon.mark").add_file() end,
+    },
+    ["<C-e>"] = {
+      function() require("harpoon.ui").toggle_quick_menu() end,
+    },
+    ["<C-h>"] = {
+      function() require("harpoon.ui").nav_file(1) end,
+    },
+    ["<C-g>"] = {
+      function() require("harpoon.ui").nav_file(2) end,
+    },
+    ["<C-n>"] = {
+      function() require("harpoon.ui").nav_file(3) end,
+    },
+    ["<C-s>"] = {
+      function() require("harpoon.ui").nav_file(4) end,
+    },
   },
   t = {
     -- setting a mapping to false will disable it
